616a063bb88fb6885e16d1660bdc4d15b8139a46
Dominic Black <dom@encore.dev>
2022-01-25 14:42:41 +0000

    Fix workflows
    This commit fixes the Github workflows to work with the new system

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 
 	"go.encore.dev/go/builder"
 )
@@ -12,7 +15,15 @@ func main() {
 	goos := flag.String("goos", "", "GOOS")
 	goarch := flag.String("goarch", "", "GOARCH")
 	dst := flag.String("dst", "dist", "build destination")
+	readVersion := flag.Bool("read-built-version", false, "If set we'll simply parse go/VERSION.cache and return the Go verison")
 	flag.Parse()
+
+	if *readVersion {
+		readBuiltVersion()
+
+		return
+	}
+
 	if *goos == "" || *goarch == "" || *dst == "" {
 		log.Fatalf("missing -dst %q, -goos %q, or -goarch %q", *dst, *goos, *goarch)
 	}
@@ -25,4 +36,25 @@ func main() {
 	if err := builder.BuildEncoreGo(*goos, *goarch, root, *dst); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func readBuiltVersion() {
+	bytes, err := ioutil.ReadFile("go/VERSION.cache")
+	if err != nil {
+		log.Fatalf("Unable to read built version: %+v", err)
+	}
+
+	str := string(bytes)
+
+	re, err := regexp.Compile("(encore-go[^ ]+)")
+	if err != nil {
+		log.Fatalf("Unable to compile regex: %+v", err)
+	}
+
+	version := re.FindString(str)
+	if version == "" {
+		log.Fatalf("Unable to extract version from `go/VERSION.cache` read: %s", str)
+	}
+
+	fmt.Println(version)
 }
