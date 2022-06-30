ef4cbf8abfb430495fbadf39f550457510e515dd
Dominic Black <dom@encore.dev>
2022-06-30 10:45:42 +0100

    Address PR comments

--- overlay/src/testing/encore_noapp.go
+++ overlay/src/testing/encore_noapp.go
@@ -3,22 +3,22 @@
 
 package testing
 
-// encoreTestStart is called when a test starts running. This allows Encore's testing framework to
-// isolate behaviour between different tests on global state.
+// encoreStartTest is called when a test starts running. This allows Encore's testing framework to
+// isolate behavior between different tests on global state.
 //
 // This implementation is simply a no-op as it's used when the tests are not being run against an Encore application
-func encoreTestStart(t *T) {}
+func encoreStartTest(t *T) {}
 
-// encoreTestEnd is called when a test ends. This allows Encore's testing framework to clear down any state from the test
+// encoreEndTest is called when a test ends. This allows Encore's testing framework to clear down any state from the test
 // and to perform any assertions on that state that it needs to. It is linked to the Encore runtime via go:linkname.
 //
 // This implementation is simply a no-op as it's used when the tests are not being run against an Encore application
-func encoreTestEnd(t *T) {}
+func encoreEndTest(t *T) {}
 
-// encoreTestPaused is called when a test is paused. This allows Encore's testing framework to
-// isolate behaviour between different tests on global state. It is linked to the Encore runtime via go:linkname.
-func encoreTestPaused(t *T) {}
+// encorePauseTest is called when a test is paused. This allows Encore's testing framework to
+// isolate behavior between different tests on global state. It is linked to the Encore runtime via go:linkname.
+func encorePauseTest(t *T) {}
 
-// encoreTestResumed is called when a test is resumed after being paused. This allows Encore's testing framework to clear down any state from the test
+// encoreResumeTest is called when a test is resumed after being paused. This allows Encore's testing framework to clear down any state from the test
 // and to perform any assertions on that state that it needs to. It is linked to the Encore runtime via go:linkname.
-func encoreTestResumed(t *T) {}
+func encoreResumeTest(t *T) {}
