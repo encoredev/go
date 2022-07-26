5391d8b72c6c8444780b96a93b7d4922de7d06cf
André Eriksson <andre@encore.dev>
2022-07-26 14:28:36 +0200

    runtime: simplify request tracking
    This simplifies the interface of Encore's request
    tracking by moving most of the logic to the appruntime
    instead of the Go runtime.

--- overlay/src/runtime/encore_app.go
+++ overlay/src/runtime/encore_app.go
@@ -3,7 +3,14 @@
 
 package runtime
 
-// encoreSendTrace is called by Encore's go runtime to send a trace.
+import "unsafe"
+
+// startEncoreG starts a new g, copying the src encore data to a new encoreG value.
 // It must be defined by the Encore runtime and linked using
 // go:linkname.
-func encoreSendTrace(log []byte)
+func startEncoreG(src unsafe.Pointer) unsafe.Pointer
+
+// exitEncoreG marks a goroutine as having exited.
+// It must be defined by the Encore runtime and linked using
+// go:linkname.
+func exitEncoreG(src unsafe.Pointer)
