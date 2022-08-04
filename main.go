7659bce6b8dd45f59ac379a2d88ad65b39139f3f
Dominic Black <dom@encore.dev>
2022-08-04 12:09:55 +0100

    Update Patches to Support Go 1.19 (#10)
    This commit updates our patches to mereg correctly with Go 1.19
    and fixes a couple of small bugs.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 
 	"go.encore.dev/go/builder"
 )
@@ -39,22 +40,43 @@ func main() {
 }
 
 func readBuiltVersion() {
-	bytes, err := ioutil.ReadFile("go/VERSION.cache")
-	if err != nil {
-		log.Fatalf("Unable to read built version: %+v", err)
+	str := ""
+	if isfile("go/VERSION") {
+		// If we're building from a release branch, we use this as the base
+		str = readfile("go/VERSION")
+		// Then we repeat the replace we do within the src/cmd/dist/build.go
+		str = strings.Replace(str, "go1.", "encore-go1.", 1)
+	} else {
+		// Otherwise we read the cache file which would be created by the build process
+		// if there was no VERSION file present
+		str = readfile("go/VERSION.cache")
 	}
 
-	str := string(bytes)
-
+	// With our patches there must always be an `encore-go1.xx` version in this string
+	// (there may be other bits, like "devel" or "beta" which we don't care about)
 	re, err := regexp.Compile("(encore-go[^ ]+)")
 	if err != nil {
 		log.Fatalf("Unable to compile regex: %+v", err)
 	}
-
 	version := re.FindString(str)
 	if version == "" {
-		log.Fatalf("Unable to extract version from `go/VERSION.cache` read: %s", str)
+		log.Fatalf("Unable to extract version, read: %s", str)
 	}
 
 	fmt.Println(version)
+}
+
+// isfile reports whether p names an existing file.
+func isfile(p string) bool {
+	fi, err := os.Stat(p)
+	return err == nil && fi.Mode().IsRegular()
+}
+
+// readfile returns the content of the named file.
+func readfile(file string) string {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	return strings.TrimRight(string(data), " \t\r\n")
 }
