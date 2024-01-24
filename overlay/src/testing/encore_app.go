c2c4e03d9ea22f2d69cafc5f3d430c652da79fe7
Dominic Black <dom@encore.dev>
2024-01-24 10:08:03 +0000

    [ENC-2013] Add support for caputring test logs (#27)

--- overlay/src/testing/encore_app.go
+++ overlay/src/testing/encore_app.go
@@ -7,7 +7,7 @@ import _ "unsafe"
 
 // encoreStartTest is called when a test starts running. This allows Encore's testing framework to
 // isolate behavior between different tests on global state. It is linked to the Encore runtime via go:linkname.
-func encoreStartTest(t *T)
+func encoreStartTest(t *T, fn func(t *T))
 
 // encoreEndTest is called when a test ends. This allows Encore's testing framework to clear down any state from the test
 // and to perform any assertions on that state that it needs to. It is linked to the Encore runtime via go:linkname.
@@ -20,3 +20,7 @@ func encorePauseTest(t *T)
 // encoreResumeTest is called when a test is resumed after being paused. This allows Encore's testing framework to clear down any state from the test
 // and to perform any assertions on that state that it needs to. It is linked to the Encore runtime via go:linkname.
 func encoreResumeTest(t *T)
+
+// encoreTestLog is called when a test logs a line. This allows Encore's testing framework to capture the log output
+// and emit that log output to the test trace.
+func encoreTestLog(line string, frameSkip int)
